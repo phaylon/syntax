f4566a21891677be6573226e485702d76966c998
Robert Sedlacek <r.sedlacek@shadowcat.co.uk>
2012-05-18 20:46:55 +0200

    whitespace fixes

--- lib/syntax.pm
+++ lib/syntax.pm
@@ -154,15 +154,15 @@ Same as L</unimport>, but will uninstall the C<@features> from C<$from>.
 =head1 DESCRIPTION
 
 This module activates community provided syntax extensions to Perl. You pass it
-a feature name, and optionally a scalar with arguments, and the dispatching 
+a feature name, and optionally a scalar with arguments, and the dispatching
 system will load and install the extension in your package.
 
-The import arguments are parsed with L<Data::OptList>. There are no 
+The import arguments are parsed with L<Data::OptList>. There are no
 standardised options. Please consult the documentation for the specific syntax
 feature to find out about possible configuration options.
 
 The passed in feature names are simply transformed: C<function> becomes
-L<Syntax::Feature::Function> and C<foo_bar> would become 
+L<Syntax::Feature::Function> and C<foo_bar> would become
 C<Syntax::Feature::FooBar>.
 
 =head1 RECOMMENDED FEATURES
